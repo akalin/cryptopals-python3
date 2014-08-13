28c5cd95cded1dc64280facd0bff38820244d8b7
Frederick Akalin <akalin@gmail.com>
2014-08-13 03:27:38 -0700

    Remove small prefix assumption for challenge14

--- challenge14.py
+++ challenge14.py
@@ -15,13 +15,25 @@ def encryption_oracle(s):
         key = challenge11.randbytes(16)
     if prefix is None:
         # TODO(akalin): Extend to arbitrary sizes.
-        randcount = random.randint(0, 16)
+        randcount = random.randint(16, 32)
         prefix = challenge11.randbytes(randcount)
     cipher = AES.new(key, AES.MODE_ECB)
     s = challenge9.padPKCS7(prefix + s + base64.b64decode(challenge12.encodedSuffix), 16)
     return cipher.encrypt(s)
 
-def findPrefixSize(encryption_oracle, blocksize):
+def getBlocks(s, blocksize):
+    return [s[i:i+blocksize] for i in range(0, len(s), blocksize)]
+
+def findPrefixBlock(encryption_oracle, blocksize):
+    x1 = encryption_oracle(b'')
+    x2 = encryption_oracle(b'0')
+    blocks1 = getBlocks(x1, blocksize)
+    blocks2 = getBlocks(x2, blocksize)
+    for i in range(len(blocks1)):
+        if blocks1[i] != blocks2[i]:
+            return i
+
+def findPrefixSizeModBlockSize(encryption_oracle, blocksize):
     def has_equal_block(blocks):
         for i in range(len(blocks) - 1):
             if blocks[i] == blocks[i+1]:
@@ -31,22 +43,26 @@ def findPrefixSize(encryption_oracle, blocksize):
     for i in range(blocksize):
         s = bytes([0] * (2*blocksize + i))
         t = encryption_oracle(s)
-        blocks = [t[i:i+blocksize] for i in range(0, len(t), blocksize)]
+        blocks = getBlocks(t, blocksize)
         if has_equal_block(blocks):
             return blocksize - i
 
     raise Exception('Not using ECB')
 
+def findPrefixSize(encryption_oracle, blocksize):
+    return blocksize*findPrefixBlock(encryption_oracle, blocksize) + findPrefixSizeModBlockSize(encryption_oracle, blocksize)
+
 def findNextByte(encryption_oracle, blocksize, prefixsize, knownBytes):
-    k1 = blocksize - prefixsize
+    k1 = blocksize - (prefixsize % blocksize)
     k2 = blocksize - (len(knownBytes) % blocksize) - 1
+    k3 = prefixsize - (prefixsize % blocksize)
     s = bytes([0] * (k1 + k2))
     d = {}
     for i in range(256):
         t = encryption_oracle(s + knownBytes + bytes([i]))
-        d[t[k1:k1+k2 + len(knownBytes) + 1]] = i
+        d[t[k3+k1:k3+k1+k2 + len(knownBytes) + 1]] = i
     t = encryption_oracle(s)
-    u = t[k1:k1+k2 + len(knownBytes) + 1]
+    u = t[k3+k1:k3+k1+k2 + len(knownBytes) + 1]
     if u in d:
         return d[u]
     return None
