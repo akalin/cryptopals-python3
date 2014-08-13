34f89f3e1d604393bc1f9ce7cf807105bddb10a0
Frederick Akalin <akalin@gmail.com>
2014-08-12 18:33:31 -0700

    Turn set1/challenge5 into a module

--- set1/challenge5.py
+++ set1/challenge5.py
@@ -9,9 +9,10 @@ key = 'ICE'
 encodedExpectedY = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
 expectedY = binascii.unhexlify(encodedExpectedY)
 
-y = encodeRepeatingKeyXor(x, key)
-encodedY = binascii.hexlify(y).decode('ascii')
-print(encodedY)
-print(encodedExpectedY)
-if y != expectedY:
-    raise Exception(encodedY + ' != ' + encodedExpectedY)
+if __name__ == '__main__':
+    y = encodeRepeatingKeyXor(x, key)
+    encodedY = binascii.hexlify(y).decode('ascii')
+    print(encodedY)
+    print(encodedExpectedY)
+    if y != expectedY:
+        raise Exception(encodedY + ' != ' + encodedExpectedY)
