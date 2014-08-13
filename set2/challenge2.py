a525d7c7a371a5c0250332346fa5444610e816e8
Frederick Akalin <akalin@gmail.com>
2014-08-12 20:22:17 -0700

    Implement encryption for challenge2

--- set2/challenge2.py
+++ set2/challenge2.py
@@ -1,5 +1,7 @@
 import base64
+import challenge1
 from Crypto.Cipher import AES
+from Crypto.Util.strxor import strxor
 
 class CBC:
     def __init__(self, ECB, IV):
@@ -7,10 +9,22 @@ class CBC:
         self._IV = IV
         self._blocksize = 16
 
+    def _getBlocks(self, s):
+        return [s[i:i+self._blocksize] for i in range(0, len(s), self._blocksize)]
+
     def encrypt(self, plaintext):
-        return plaintext
+        plainblocks = self._getBlocks(challenge1.padPKCS7(plaintext, self._blocksize))
+        ciphertext = b''
+        prev = self._IV
+        for i in range(len(plainblocks)):
+            plainblock = plainblocks[i]
+            cipherblock = self._ECB.encrypt(strxor(plainblock, prev))
+            ciphertext += cipherblock
+            prev = cipherblock
+        return ciphertext
 
     def decrypt(self, ciphertext):
+        cipherblocks = self._getBlocks(ciphertext)
         return ciphertext
 
 x = base64.b64decode(open('10.txt', 'r').read())
