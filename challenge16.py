ef56c3fd4cd6f214118a977a1ca03043e3c9b722
Frederick Akalin <akalin@gmail.com>
2014-08-13 04:06:39 -0700

    Add outline for challenge16

---
+++ challenge16.py
@@ -0,0 +1,26 @@
+from Crypto.Cipher import AES
+import challenge9
+import challenge10
+import challenge11
+import challenge15
+
+key = challenge11.randbytes(16)
+iv = challenge11.randbytes(16)
+
+def encryptParams(userdata):
+    userdata = userdata.replace(';', '%3B').replace('=', '%3D')
+    x1 = b'comment1=cooking%20MCs;userdata='
+    x2 = b';comment2=%20like%20a%20pound%20of%20bacon'
+    params = x1 + userdata.encode('ascii') + x2
+    cipher = challenge10.CBC(AES.new(key, AES.MODE_ECB), iv)
+    return cipher.encrypt(challenge9.padPKCS7(params, 16))
+
+def decryptParamsAndCheckAdmin(encryptedParams):
+    cipher = challenge10.CBC(AES.new(key, AES.MODE_ECB), iv)
+    paddedParams = cipher.decrypt(encryptedParams)
+    params = challenge15.unpadPKCS7(paddedParams)
+    return params.find(b';admin=true;') != -1
+
+x = encryptParams('userdata')
+# TODO(akalin): Munge x.
+print(decryptParamsAndCheckAdmin(x))
