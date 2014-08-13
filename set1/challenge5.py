9d60fb936129b1639600a67e5af42b31ffcb04c5
Frederick Akalin <akalin@gmail.com>
2014-08-12 18:34:47 -0700

    Make encodeRepeatingKeyXor() take bytes

--- set1/challenge5.py
+++ set1/challenge5.py
@@ -1,11 +1,11 @@
 import binascii
 
 def encodeRepeatingKeyXor(s, key):
-    return bytes([ord(s[i]) ^ ord(key[i % len(key)]) for i in range(len(s))])
+    return bytes([s[i] ^ key[i % len(key)] for i in range(len(s))])
 
-x = '''Burning 'em, if you ain't quick and nimble
+x = b'''Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal'''
-key = 'ICE'
+key = b'ICE'
 encodedExpectedY = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
 expectedY = binascii.unhexlify(encodedExpectedY)
 
