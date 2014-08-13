6c25d363cff0f2bde91fe1e7fadfb408990765af
Frederick Akalin <akalin@gmail.com>
2014-08-13 04:03:58 -0700

    Convert challenge15 into a module

--- challenge15.py
+++ challenge15.py
@@ -4,14 +4,15 @@ def unpadPKCS7(s):
         raise ValueError('bad padding')
     return s[0:-i]
 
-print(unpadPKCS7(b'ICE ICE BABY\x04\x04\x04\x04'))
-try:
-    unpadPKCS7(b'ICE ICE BABY\x05\x05\x05\x05')
-    raise Exception('passes unexpectedly')
-except ValueError:
-    pass
-try:
-    unpadPKCS7(b'ICE ICE BABY\x01\x02\x03\x04')
-    raise Exception('passes unexpectedly')
-except ValueError:
-    pass
+if __name__ == '__main__':
+    print(unpadPKCS7(b'ICE ICE BABY\x04\x04\x04\x04'))
+    try:
+        unpadPKCS7(b'ICE ICE BABY\x05\x05\x05\x05')
+        raise Exception('passes unexpectedly')
+    except ValueError:
+        pass
+    try:
+        unpadPKCS7(b'ICE ICE BABY\x01\x02\x03\x04')
+        raise Exception('passes unexpectedly')
+    except ValueError:
+        pass
