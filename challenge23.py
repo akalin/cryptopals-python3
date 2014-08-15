6eba76cfd18cb225ad5ce14c7cc51af91845a79d
Frederick Akalin <akalin@gmail.com>
2014-08-15 00:42:32 -0700

    Implement challenge23

--- challenge23.py
+++ challenge23.py
@@ -1,3 +1,6 @@
+from Crypto.Random import random
+import challenge21
+
 def getMSB(x, n):
     if n < 0:
         return 0
@@ -32,3 +35,17 @@ def untemper(y):
     y = undoLeftShiftXorAnd(y, 7, 0x9d2c5680)
     y = undoRightShiftXor(y, 11)
     return y
+
+seed = random.getrandbits(32)
+rng = challenge21.MT19937(seed)
+MT = [0] * 624
+for i in range(624):
+    MT[i] = untemper(rng.uint32())
+rng2 = challenge21.MT19937(0)
+rng2._MT = MT
+
+for i in range(1000):
+    a = rng.uint32()
+    b = rng2.uint32()
+    if a != b:
+        raise Exception(str(i) + ' ' + str(a) + ' != ' + str(b))
