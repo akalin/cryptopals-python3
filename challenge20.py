a638aaced3623c137c061e91bf62d3318e11a9f8
Frederick Akalin <akalin@gmail.com>
2014-08-14 18:22:14 -0700

    Extend key further for challenge20

--- challenge20.py
+++ challenge20.py
@@ -16,6 +16,13 @@ def breakSameKey(strings):
 
 key = breakSameKey(encryptedStrings)
 key = bytes([encryptedStrings[0][0] ^ ord('I')]) + key[1:]
+key = challenge19.extendKey(key, encryptedStrings[13], b'-M ')
+key = challenge19.extendKey(key, encryptedStrings[16], b'ime ')
+key = challenge19.extendKey(key, encryptedStrings[1], b'htnin')
+key = challenge19.extendKey(key, encryptedStrings[2], b'y')
+key = challenge19.extendKey(key, encryptedStrings[6], b'ty')
+key = challenge19.extendKey(key, encryptedStrings[0], b'i')
+key = challenge19.extendKey(key, encryptedStrings[3], b'n up')
 kl = len(key)
 for s in encryptedStrings:
     print(strxor(s[:kl], key[:len(s)]) + s[kl:])
