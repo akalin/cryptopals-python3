6e8a77a3e4928195e902d20cada0484f3f1ad223
Frederick Akalin <akalin@gmail.com>
2014-08-15 00:38:00 -0700

    Implement undoLeftShiftXorAnd() for challenge23

--- challenge23.py
+++ challenge23.py
@@ -11,3 +11,17 @@ def undoRightShiftXor(y, s):
     for i in range(32):
         z = setMSB(z, i, getMSB(y, i) ^ getMSB(z, i - s))
     return z
+
+def getLSB(x, n):
+    if n < 0:
+        return 0
+    return (x >> n) & 1
+
+def setLSB(x, n, b):
+    return x | (b << n)
+
+def undoLeftShiftXorAnd(y, s, k):
+    z = 0
+    for i in range(32):
+       z = setLSB(z, i, getLSB(y, i) ^ (getLSB(z, i - s) & getLSB(k, i)))
+    return z
