deee56970755503665e386a05ede90df87cb71cc
Frederick Akalin <akalin@gmail.com>
2014-08-15 22:09:52 -0700

    Implement guessNextByte() for challenge31

--- challenge31.py
+++ challenge31.py
@@ -17,7 +17,27 @@ def isValidSignature(file, signature):
             raise
         return (False, end - start)
 
+def guessNextByte(file, knownBytes):
+    suffixlen = 20 - len(knownBytes)
+    expectedDuration = 0.05 * len(knownBytes) + 0.01
+    start = time.perf_counter()
+
+    def print_info(i):
+        end = time.perf_counter()
+        print('Made {0} requests in {1:.2f}s; rps = {2:.2f}, max rps = {3:.2f}'.format(i, end - start, i / (end - start), 1 / expectedDuration))
+
+    for i in range(256):
+        suffix = bytes([i]) + (b'\x00' * (suffixlen - 1))
+        signature = knownBytes + suffix
+        _, duration = isValidSignature(file, binascii.hexlify(signature).decode('ascii'))
+        if duration > expectedDuration + 0.04:
+            print_info(i)
+            return knownBytes + bytes([i])
+
+    raise Exception('unexpected')
+
 if __name__ == '__main__':
     file = sys.argv[1]
-    signature = '00'
-    print(isValidSignature(file, signature))
+    knownBytes = b''
+    knownBytes = guessNextByte(file, knownBytes)
+    print(binascii.hexlify(knownBytes))
