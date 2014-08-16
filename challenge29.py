733e77f64aae00de9508fa3d8fe993e88df9129a
Frederick Akalin <akalin@gmail.com>
2014-08-15 19:28:28 -0700

    Outline rest of challenge29

--- challenge29.py
+++ challenge29.py
@@ -1,3 +1,6 @@
+from Crypto.Random import random
+import challenge11
+import challenge28
 import struct
 
 def padSHA1(s):
@@ -6,3 +9,25 @@ def padSHA1(s):
     s += b'\x00' * ((56 - (len(s) % 64)) % 64)
     s += struct.pack('>Q', l)
     return s
+
+keylen = random.randint(0, 100)
+key = challenge11.randbytes(keylen)
+
+def validate(message, digest):
+    return challenge28.authSHA1(key, message) == digest
+
+message = b'comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon'
+messageDigest = challenge28.authSHA1(key, message)
+
+def forgeHash(keylen, message, digest, suffix):
+    # TODO(akalin): Implement.
+    return (message + suffix, b'')
+
+def forgeValidatingHash(maxkeylen, message, digest, suffix):
+    for i in range(maxkeylen):
+        (forgedMessage, forgedDigest) = forgeHash(i, message, digest, suffix)
+        if validate(forgedMessage, forgedDigest):
+            return(forgedMessage, forgedDigest)
+    raise Exception('unexpected')
+
+print(forgeValidatingHash(100, message, messageDigest, b';admin=true'))
