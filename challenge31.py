f741017fd22689e0a8c86ba54526bb8a396a2fdc
Frederick Akalin <akalin@gmail.com>
2014-08-15 21:45:45 -0700

    Add timing information for challenge31

--- challenge31.py
+++ challenge31.py
@@ -1,17 +1,21 @@
 import binascii
 import sys
+import time
 import urllib.request
 
 def isValidSignature(file, signature):
+    start = time.perf_counter()
     try:
         response = urllib.request.urlopen('http://localhost:9000/test?file=' + file + '&signature=' + signature)
+        end = time.perf_counter()
         if response.status != 200:
             raise Exception('unexpected status ' + str(response.status))
-        return True
+        return (True, end - start)
     except urllib.error.HTTPError as e:
+        end = time.perf_counter()
         if e.code != 500:
-            raise Exception('unexpected status ' + str(e.code))
-        return False
+            raise
+        return (False, end - start)
 
 if __name__ == '__main__':
     file = sys.argv[1]
