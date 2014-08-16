dce399a10d0cf8f5f3e5670aad63d83e819e4f36
Frederick Akalin <akalin@gmail.com>
2014-08-15 19:32:45 -0700

    Add h{0,1,2,3,4} and length parameters to SHA1 class

--- challenge28.py
+++ challenge28.py
@@ -1,10 +1,17 @@
 # Taken from https://github.com/sfstpala/SlowSHA
 class SHA1 (object):
-    _h0, _h1, _h2, _h3, _h4, = (
+    _default_h0, _default_h1, _default_h2, _default_h3, _default_h4, = (
         0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0)
 
-    def __init__(self, message):
-        length = bin(len(message) * 8)[2:].rjust(64, "0")
+    def __init__(self, message, h0 = _default_h0, h1 = _default_h1, h2 = _default_h2, h3 = _default_h3, h4 = _default_h4, length = None):
+        self._h0 = h0
+        self._h1 = h1
+        self._h2 = h2
+        self._h3 = h3
+        self._h4 = h4
+        if length is None:
+            length = len(message) * 8
+        length = bin(length)[2:].rjust(64, "0")
         while len(message) > 64:
             self._handle(''.join(bin(i)[2:].rjust(8, "0")
                 for i in message[:64]))
