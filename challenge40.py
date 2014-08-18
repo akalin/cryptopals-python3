5b63d8bfd8c2693f94d0b2d04ab915abdd952169
Frederick Akalin <akalin@gmail.com>
2014-08-18 00:58:19 -0700

    Implement challenge40 for numbers

---
+++ challenge40.py
@@ -0,0 +1,45 @@
+import challenge39
+import binascii
+import math
+
+(pub0, priv0) = challenge39.genKey(128)
+(pub1, priv1) = challenge39.genKey(128)
+(pub2, priv2) = challenge39.genKey(128)
+
+plainnum = 42
+
+c0 = challenge39.encryptnum(pub0, plainnum)
+c1 = challenge39.encryptnum(pub1, plainnum)
+c2 = challenge39.encryptnum(pub2, plainnum)
+
+n0 = pub0[1]
+n1 = pub1[1]
+n2 = pub2[1]
+
+ms0 = n1 * n2
+ms1 = n0 * n2
+ms2 = n0 * n1
+
+N = n0 * n1 * n2
+
+r0 = (c0 * ms0 * challenge39.invmod(ms0, n0))
+r1 = (c1 * ms1 * challenge39.invmod(ms1, n1))
+r2 = (c2 * ms2 * challenge39.invmod(ms2, n2))
+
+r = (r0 + r1 + r2) % N
+
+def floorRoot(n, s):
+    b = n.bit_length()
+    p = math.ceil(b/s)
+    x = 2**p
+    while x > 1:
+        y = (((s - 1) * x) + (n // (x**(s-1)))) // s
+        if y >= x:
+            return x
+        x = y
+    return 1
+
+m = floorRoot(r, 3)
+
+if m != plainnum:
+    raise Exception(str(m) + ' != ' + str(plainnum))
