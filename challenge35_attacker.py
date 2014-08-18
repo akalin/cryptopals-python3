e48a0bbb1048eb01ff3a0ae95ff5cf02d81df7f2
Frederick Akalin <akalin@gmail.com>
2014-08-17 20:19:01 -0700

    Implement attack for g = p

--- challenge35_attacker.py
+++ challenge35_attacker.py
@@ -5,11 +5,13 @@ import sys
 
 targethost = ''
 targetport = 0
+targetg = 0
 
 class AttackerTCPHandler(socketserver.StreamRequestHandler):
     def handle(self):
         global targethost
         global targetport
+        global targetg
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
@@ -26,8 +28,13 @@ class AttackerTCPHandler(socketserver.StreamRequestHandler):
             print('A->S: writing p...')
             serverutil.writenum(p)
 
-            print('A->S: writing 1...')
-            serverutil.writenum(1)
+            if targetg > 0:
+                fakeg = 1
+            else:
+                fakeg = p
+
+            print('A->S: writing fake g...')
+            serverutil.writenum(fakeg)
 
             print('S->A: reading p...')
             serverutil.readnum()
@@ -38,14 +45,14 @@ class AttackerTCPHandler(socketserver.StreamRequestHandler):
             print('A->C: writing p...')
             clientutil.writenum(p)
 
-            print('A->C: writing 1...')
-            clientutil.writenum(1)
+            print('A->C: writing fake g...')
+            clientutil.writenum(fakeg)
 
             print('C->A: reading A...')
             A = clientutil.readnum()
 
-            print('A->S: writing 1...')
-            serverutil.writenum(1)
+            print('A->S: writing A...')
+            serverutil.writenum(A)
 
             print('S->A: reading B...')
             B = serverutil.readnum()
@@ -77,7 +84,11 @@ class AttackerTCPHandler(socketserver.StreamRequestHandler):
             print('A->C: writing iv...')
             clientutil.writebytes(iv2)
 
-            key = serverutil.derivekey(1)
+            if targetg > 0:
+                s = 1
+            else:
+                s = 0
+            key = serverutil.derivekey(s)
             message = serverutil.decrypt(key, iv, encryptedMessage)
 
             print('A: message: ' + message)
@@ -90,6 +101,7 @@ if __name__ == "__main__":
     port = int(sys.argv[2])
     targethost = sys.argv[3]
     targetport = int(sys.argv[4])
+    targetg = int(sys.argv[5])
 
     print('listening on ' + host + ':' + str(port) + ', attacking ' + targethost + ':' + str(targetport))
     socketserver.TCPServer.allow_reuse_address = True
