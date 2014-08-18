2eb9f83336ac390ad314ae33de8daee5548d2bc5
Frederick Akalin <akalin@gmail.com>
2014-08-17 19:58:04 -0700

    Make attacker for challenge34 use util

--- challenge34_attacker.py
+++ challenge34_attacker.py
@@ -1,9 +1,5 @@
-from Crypto.Cipher import AES
 from Crypto.Random import random
-import base64
-import challenge9
-import challenge15
-import hashlib
+import challenge34_util
 import socket
 import socketserver
 import sys
@@ -11,52 +7,7 @@ import sys
 targethost = ''
 targetport = 0
 
-def readline(f):
-    return f.readline().strip()
-
-def readnum(f):
-    return int(readline(f))
-
-def readbytes(f):
-    return base64.b64decode(readline(f))
-
-def writeline(f, line):
-    f.write(line + b'\n')
-
-def writenum(f, num):
-    writeline(f, str(num).encode('ascii'))
-
-def writebytes(f, bytes):
-    writeline(f, base64.b64encode(bytes))
-
-def derivekey(s):
-    sha1 = hashlib.sha1()
-    sha1.update(str(s).encode('ascii'))
-    return sha1.digest()[:16]
-
-def decrypt(key, iv, encryptedMessage):
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return challenge15.unpadPKCS7(cipher.decrypt(encryptedMessage)).decode('ascii')
-
 class AttackerTCPHandler(socketserver.StreamRequestHandler):
-    def readline(self):
-        return self.rfile.readline().strip()
-
-    def readnum(self):
-        return int(self.readline())
-
-    def readbytes(self):
-        return base64.b64decode(self.readline())
-
-    def writeline(self, line):
-        self.wfile.write(line + b'\n')
-
-    def writenum(self, num):
-        self.writeline(str(num).encode('ascii'))
-
-    def writebytes(self, bytes):
-        self.writeline(base64.b64encode(bytes))
-
     def handle(self):
         global targethost
         global targetport
@@ -64,58 +15,59 @@ class AttackerTCPHandler(socketserver.StreamRequestHandler):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             sock.connect((targethost, targetport))
-            f = sock.makefile(mode='rwb', buffering=0)
+            serverutil = challenge34_util.Util(sock)
+            clientutil = challenge34_util.Util(self)
 
             print('C->A: reading p...')
-            p = self.readnum()
+            p = clientutil.readnum()
 
             print('C->A: reading g...')
-            g = self.readnum()
+            g = clientutil.readnum()
 
             print('C->A: reading A...')
-            A = self.readnum()
+            A = clientutil.readnum()
 
             print('A->S: writing p...')
-            writenum(f, p)
+            serverutil.writenum(p)
 
             print('A->S: writing g...')
-            writenum(f, g)
+            serverutil.writenum(g)
 
             print('A->S: writing p...')
-            writenum(f, p)
+            serverutil.writenum(p)
 
             print('S->A: reading B...')
-            B = readnum(f)
+            B = serverutil.readnum()
 
             print('A->C: writing p...')
-            self.writenum(p)
+            clientutil.writenum(p)
 
             print('C->A: reading encrypted message...')
-            encryptedMessage = self.readbytes()
+            encryptedMessage = clientutil.readbytes()
 
             print('A->S: writing encrypted message...')
-            writebytes(f, encryptedMessage)
+            serverutil.writebytes(encryptedMessage)
 
             print('C->A: reading iv...')
-            iv = self.readbytes()
+            iv = clientutil.readbytes()
 
             print('A->S: writing iv...')
-            writebytes(f, iv)
+            serverutil.writebytes(iv)
 
             print('S->A: reading encrypted message...')
-            encryptedMessage2 = readbytes(f)
+            encryptedMessage2 = serverutil.readbytes()
 
             print('A->C: writing encrypted message...')
-            self.writebytes(encryptedMessage2)
+            clientutil.writebytes(encryptedMessage2)
 
             print('S->A: reading iv...')
-            iv2 = readbytes(f)
+            iv2 = serverutil.readbytes()
 
             print('A->C: writing iv...')
-            self.writebytes(iv2)
+            clientutil.writebytes(iv2)
 
-            key = derivekey(0)
-            message = decrypt(key, iv, encryptedMessage)
+            key = serverutil.derivekey(0)
+            message = serverutil.decrypt(key, iv, encryptedMessage)
 
             print('A: message: ' + message)
 
