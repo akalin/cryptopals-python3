679313d81a718eaa5c10ea05e876a3145bc7d49a
Frederick Akalin <akalin@gmail.com>
2014-08-20 23:29:40 -0700

    Implement challenge42

--- challenge42.py
+++ challenge42.py
@@ -1,4 +1,5 @@
 import challenge39
+import challenge40
 import hashlib
 import re
 
@@ -21,9 +22,23 @@ def verifySignature(pub, message, signature):
     sha1.update(message)
     return digest == sha1.digest()
 
+def forgeSignature(pub, message):
+    sha1 = hashlib.sha1()
+    sha1.update(message)
+    digest = sha1.digest()
+    block = b'\x00\x01\xff\x00' + digest + (b'\x00' * (128 - len(digest) - 4))
+    (d, n) = pub
+    blocknum = challenge39.bytestonum(block)
+    signaturenum = challenge40.floorRoot(blocknum, 3) + 1
+    signature = challenge39.numtobytes(signaturenum)
+    return signature
+
 if __name__ == '__main__':
     message = b'hi mom'
     pub, priv = challenge39.genKey(1024)
     signature = generateSignature(priv, message)
     if not verifySignature(pub, message, signature):
         raise Exception(message + b' has invalid signature ' + signature)
+    forgedSignature = forgeSignature(pub, message)
+    if not verifySignature(pub, message, forgedSignature):
+        raise Exception(message + b' has invalid signature ' + forgedSignature)
