1b128f4268568384b7c3d154db1cd9724c1a51e6
Frederick Akalin <akalin@gmail.com>
2014-08-21 17:27:04 -0700

    Implement DSA key generation for challenge43

--- challenge43.py
+++ challenge43.py
@@ -31,10 +31,22 @@ def areValidParams(L, N, p, q, g):
             ((p-1) % q == 0) and
             pow(g, q, p) == 1)
 
+def genKeys(p, q, g):
+    x = random.randint(1, q-1)
+    y = pow(g, x, p)
+    pub = (p, q, g, y)
+    priv = x
+    return (pub, priv)
+
+def areValidKeys(pub, priv):
+    (p, q, g, y) = pub
+    x = priv
+    return y == pow(g, x, p)
+
 if __name__ == '__main__':
     L = 1024
     N = 160
     (p, q, g) = (0x800000000000000089e1855218a0e7dac38136ffafa72eda7859f2171e25e65eac698c1702578b07dc2a1076da241c76c62d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebeac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc871a584471bb1, 0xf4f47f05794b256174bba6e9b396a7707e563c5b, 0x5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119458fef538b8fa4046c8db53039db620c094c9fa077ef389b5322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a0470f5b64c36b625a097f1651fe775323556fe00b3608c887892878480e99041be601a62166ca6894bdd41a7054ec89f756ba9fc95302291)
     print(p, q, g, areValidParams(L, N, p, q, g))
-    (p, q, g) = genParams(L, N)
-    print(p, q, g, areValidParams(L, N, p, q, g))
+    (pub, priv) = genKeys(p, q, g)
+    print(pub, priv, areValidKeys(pub, priv))
