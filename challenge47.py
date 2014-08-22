765d80aa82ae36e917a1e02fedbe3e7b84a2492a
Frederick Akalin <akalin@gmail.com>
2014-08-22 00:23:34 -0700

    Implement step 2a for challenge47

--- challenge47.py
+++ challenge47.py
@@ -21,7 +21,22 @@ def padPKCS15(s, n):
     padding = randnonzerobytes(k - 3 - len(s))
     return b'\x00\x02' + padding + b'\x00' + s
 
+def computeFirstS(e, n, B, c0, parityOracle):
+    s = (n + 3*B - 1) // (3*B)
+    while True:
+        c = (c0 * pow(s, e, n)) % n
+        if parityOracle(c):
+            return (s, c)
+        s += 1
+
 def deducePlaintext(ciphertext, pub, parityOracle):
+    e, n = pub
+    k = (n.bit_length() + 7) // 8
+    B = 2**(8*(k-2))
+    c0 = challenge39.bytestonum(ciphertext)
+    M = [(2*B, 3*B - 1)]
+    (s, c) = computeFirstS(e, n, B, c0, parityOracle)
+    print(s, c)
     # TODO(akalin): Deduce plaintext using parityOracle.
     return b''
 
