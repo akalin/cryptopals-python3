c330ca7382a3564bf8a12c86523d0d96e566c244
Frederick Akalin <akalin@gmail.com>
2014-09-06 14:39:51 -0700

    Generate collisions with only printable characters for challenge50

--- challenge50.py
+++ challenge50.py
@@ -1,6 +1,8 @@
 from Crypto.Util.strxor import strxor
 import binascii
 import challenge49
+import itertools
+import string
 import util
 
 def insecureHash(s):
@@ -11,10 +13,22 @@ def prependAndCollide(s, prefix):
     paddedPrefix = util.padPKCS7(prefix, 16)
     return paddedPrefix + strxor(s[:16], prefixHash) + s[16:]
 
+def prependAndCollidePrintable(s, prefix):
+    # Ensure that the padding character is \t (== 0x09).
+    numFreeBytes = (7 - len(prefix)) % 16
+    if numFreeBytes == 0:
+        numFreeBytes = 16
+    for freeBytes in itertools.product(range(32, 127), repeat=numFreeBytes):
+        paddedPrefix = prefix + bytes(freeBytes)
+        collision = prependAndCollide(s, paddedPrefix)
+        scrambledBytes = collision[-len(s):-len(s) + 16]
+        if all(c >= 32 and c < 127 for c in scrambledBytes):
+            return collision
+
 s = b"alert('MZA who was that?');\n"
 sHash = insecureHash(s)
 print(binascii.hexlify(sHash))
 
 prefix = b"alert('Ayo, the Wu is back!'); //"
-collision = prependAndCollide(s, prefix)
+collision = prependAndCollidePrintable(s, prefix)
 print(collision, binascii.hexlify(insecureHash(collision)))
