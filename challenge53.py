9c343b25319428f7783a6b1a2b877f6c08e8603d
Frederick Akalin <akalin@gmail.com>
2014-10-01 02:04:46 -0700

    Implement challenge53

--- challenge53.py
+++ challenge53.py
@@ -1,4 +1,5 @@
 import challenge52
+import util
 
 def findNBlockPrefixCollision(hashFn, iv, blockLength, n):
     prefix = b'\x00' * (blockLength * (n-1))
@@ -34,12 +35,49 @@ def makeExpandedPrefix(blockSize, blocks, k, l):
         raise Exception('unexpected')
     return m
 
+def getIntermediateStates(m, hashFn, iv, blockLength):
+    state = iv
+    for block in (m[blockLength*i:blockLength*(i+1)] for i in range(len(m) // blockLength)):
+        state = hashFn(block, state, pad=False)
+        yield state
+
+def findIntermediateStateCollision(hashFn, iv, blockLength, hashLength, intermediateStateIter, minBlockCount):
+    statesToIndices = {}
+    i = 0
+    for state in intermediateStateIter:
+        if i >= minBlockCount:
+            statesToIndices[state] = i
+        i += 1
+
+    if not statesToIndices:
+        raise Exception('unexpected')
+
+    for s in (i.to_bytes(blockLength, byteorder='little') for i in range(2**(blockLength*8))):
+        h = hashFn(s, iv, pad=False)
+        if h in statesToIndices:
+            return s, statesToIndices[h]
+
+    raise Exception('unexpected')
+
+def findSecondPreimage(m, hashFn, iv, blockLength, hashLength):
+    blockCount = (len(m) + blockLength - 1) // blockLength
+    k = blockCount.bit_length()
+    prefixState, blocks = makeExpandablePrefix(hashFn, iv, blockLength, k)
+
+    intermediateStateIter = getIntermediateStates(m, hashFn, iv, blockLength)
+    bridge, collisionBlockCount = findIntermediateStateCollision(hashFn, prefixState, blockLength, hashLength, intermediateStateIter, k)
+
+    m2 = makeExpandedPrefix(blockLength, blocks, k, collisionBlockCount) + bridge
+    m2 += m[len(m2):]
+    return m2
+
 if __name__ == '__main__':
-    k = 5
-    state, blocks = makeExpandablePrefix(challenge52.badHash, b'', challenge52.badHashBlockLength, k)
-    for i in range(k, k + 2**k - 1):
-        m = makeExpandedPrefix(challenge52.badHashBlockLength, blocks, k, i)
-        mState = challenge52.badHash(m, b'', pad=False)
-        print(m, mState)
-        if state != mState:
-            raise Exception(state + b' != ' + mState)
+    m = util.randbytes(100)
+    h = challenge52.badHash(m, b'')
+    m2 = findSecondPreimage(m, challenge52.badHash, b'', challenge52.badHashBlockLength, challenge52.badHashHashLength)
+    h2 = challenge52.badHash(m2, b'')
+    print(m, m2, h, h2)
+    if len(m2) != len(m):
+        raise Exception('{0} != {1}'.format(len(m2), len(m)))
+    if h2 != h:
+        raise Exception(h2 + b' != ' + h)
