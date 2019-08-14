6ef4b0a2d6c56c8931d641443be1559260b65b3d
Frederick Akalin <akalin@gmail.com>
2019-08-14 16:07:38 -0400

    Just keep track of char frequencies

--- challenge56.py
+++ challenge56.py
@@ -12,46 +12,51 @@ def encryption_oracle(b):
     cipher = ARC4.new(key)
     return cipher.encrypt(b + cookie)
 
-def append_ciphertexts(b, S, C):
-    for _ in range(S):
-        C.append(encryption_oracle(b))
+def add_to_pos_distributions(b, iter_length, pos_distributions):
+    for _ in range(iter_length):
+        ciphertext = encryption_oracle(b)
+        for pos, distribution in pos_distributions.items():
+            distribution[ciphertext[pos]] += 1
 
-# Algorithm 3 from the paper.
-def recover_ciphertext_byte(C, r):
-    N = [0] * 256
-    for c in C:
-        N[c[r]] += 1
-    return max(range(256), key=lambda i: N[i])
-
-def recover_plaintext_byte(C, r, bias):
-    cb = recover_ciphertext_byte(C, r)
+def recover_plaintext_byte(distribution, bias):
+    cb = max(range(256), key=lambda i: distribution[i])
     return cb ^ bias
 
-def recover_plaintext_iter(ciphertext_length, iter_length, paddings, ciphertexts):
+def recover_plaintext_iter(ciphertext_length, iter_length, paddings, pos_distribution_groups):
     plaintext = [0] * ciphertext_length
 
-    for i, C in enumerate(ciphertexts):
-        append_ciphertexts(paddings[i], iter_length, C)
+    for i, pos_distributions in enumerate(pos_distribution_groups):
+        add_to_pos_distributions(paddings[i], iter_length, pos_distributions)
 
-    for i, C in enumerate(ciphertexts):
-        b15 = recover_plaintext_byte(C, 15, 240)
+    for i, pos_distributions in enumerate(pos_distribution_groups):
+        b15 = recover_plaintext_byte(pos_distributions[15], 240)
         plaintext[15 - i] = b15
         # print('plaintext[{}] = {}'.format(15 - i, bytes([b15])))
         if i + ciphertext_length >= 32:
-            b31 = recover_plaintext_byte(C, 31, 224)
+            b31 = recover_plaintext_byte(pos_distributions[31], 224)
             plaintext[31 - i] = b31
             # print('plaintext[{}] = {}'.format(31 - i, bytes([b31])))
 
-    return len(ciphertexts[0]), plaintext
+    return plaintext
 
 def recover_plaintext(iter_length, num_iters):
     ciphertext_length = len(encryption_oracle(b''))
     paddings = [bytes([0] * i) for i in range(16)]
-    ciphertexts = [[] for padding in paddings]
+
+    # Each entry in pos_distribution_groups (named pos_distributions)
+    # is a map from a ciphertext character position (15 or 31) to a
+    # histogram of ciphertext character frequencies.
+    pos_distribution_groups = []
+    for i, padding in enumerate(paddings):
+        pos_distributions = {15: [0] * 256}
+        if i + ciphertext_length >= 32:
+            pos_distributions[31] = [0] * 256
+        pos_distribution_groups.append(pos_distributions)
 
     for i in range(num_iters):
-        ciphertext_count, plaintext = recover_plaintext_iter(ciphertext_length, iter_length, paddings, ciphertexts)
-        print('with {} ciphertexts, plaintext = {}'.format(ciphertext_count, bytes(plaintext)))
+        plaintext = recover_plaintext_iter(ciphertext_length, iter_length, paddings, pos_distribution_groups)
+        ciphertexts_per_pair = sum(pos_distribution_groups[0][15])
+        print('with {} ciphertexts per 2 positions, plaintext = {}'.format(ciphertexts_per_pair, bytes(plaintext)))
 
     return plaintext
 
